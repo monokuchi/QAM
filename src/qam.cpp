1852dc769e433cea269d3970682931d190c4a78b
monokuchi <22529234+monokuchi@users.noreply.github.com>
2022-07-31 14:18:23 -0400

    Changed carrier generation

--- src/qam.cpp
+++ src/qam.cpp
@@ -6,14 +6,14 @@ int main()
 {
     // Set the Signal class variables
     std::string name = "Input Signal"; 
-    float freq = 20; // Frequency of carrier signal in Hz
-    float sample_rate = 1000; // Sample rate in Hz
+    float freq = 100; // Frequency of carrier signal in Hz
+    float sample_rate = 10000; // Sample rate in Hz
     float symbol_rate = 2; // Symbol rate in symbols/sec
     int num_bits = 16; // Make sure this is divisible by 2
     int mod_order = 4; // mod_order of 4 means we are implementing QPSK for this particular example
 
     // Make the Signal object
-    Signal signal(name, freq, sample_rate, symbol_rate, num_bits, mod_order);
+    Signal signal(name, num_bits, mod_order);
 
     // Print the signal's name
     std::cout<<"Signal Name: "<<signal.getName()<<std::endl;
@@ -22,15 +22,14 @@ int main()
     signal.generateBits();
     std::vector<int> bit_vector = signal.getBitVector();
     // Print out all the bits that make up the signal
-    std::cout<<"Bit Vector: ";
-    printOutVector(bit_vector);
+    printOutVector(bit_vector, "Bit Vector");
 
-    // Generate our carrier signal
-    signal.generateCarrier(100);
-    std::vector<float> carrier_signal = signal.getCarrierSignal();
+    // Generate our carrier signals
+    std::vector<float> I_carrier_signal = generateSin(freq, 5000, sample_rate);
+    std::vector<float> Q_carrier_signal = generateCos(freq, 5000, sample_rate);
 
     // Modulate the signal using QAM
-    std::vector<float> output = modulateQAM(bit_vector, carrier_signal);
+    std::vector<float> output = modulateQAM(bit_vector, I_carrier_signal, Q_carrier_signal);
     
     
 }
--- src/qam.h
+++ src/qam.h
@@ -8,50 +8,25 @@
 
 
 
-template <typename T>
-void printOutVector(std::vector<T> &vec)
-{
-    for (auto i = vec.begin(); i != vec.end(); ++i)
-    {
-        if (i != vec.begin()) std::cout<<", ";
-        std::cout<<*i;
-    }
-    std::cout<<"\n";
-}
-
-
 class Signal
 {
     private:
-        // Private variables
         std::string name; // Name of the signal
         std::vector<int> bit_vector; // Bit vector that makes up the signal
-        std::vector<float> carrier_signal; // Carrier signal that we mix our I and Q signals with
-
-        float frequency; // Frequency of the carrier signal in Hz
-        float sample_rate; // Sample rate in Hz
-        float symbol_rate; // Symbol rate in symbols/sec
 
         int num_bits; // Total number of bits we want to send
         int modulation_order; // Modulation order which is the number of symbols on constellation diagram
         int symbol_bits; // Number of bits for each symbol
         
     public:
-        // Constructor
-        Signal(std::string Name, float Frequency, float SampleRate, float SymbolRate, int NumBits, int ModOrder)
+        Signal(std::string Name, int NumBits, int ModOrder)
         {
             name = Name;
-            frequency = Frequency;
-            sample_rate = SampleRate;
-            symbol_rate = SymbolRate;
             num_bits = NumBits;
             modulation_order = ModOrder;
             symbol_bits = log2(modulation_order);
-            sample_rate = SampleRate;
-            symbol_rate = SymbolRate;
         }
 
-        // Public functions
         std::string getName()
         {
             return name;
@@ -60,11 +35,6 @@ class Signal
         std::vector<int> getBitVector()
         {
             return bit_vector;
-        }
-
-        std::vector<float> getCarrierSignal()
-        {
-            return carrier_signal;
         }
 
         void generateBits()
@@ -77,19 +47,49 @@ class Signal
             }
         }
 
-        void generateCarrier(int sample_size)
-        {   
-            for (float t=0; t<sample_size; t+=(1/sample_rate))
-            {
-                float sin_point = sin(2 * M_PI * frequency/sample_size * t);
-                carrier_signal.push_back(sin_point);
-            }
-        }
-
 };
 
 
-std::vector<float> modulateQAM(std::vector<int> &data, std::vector<float> &carrier)
+template <typename T>
+void printOutVector(std::vector<T> &vec, std::string Name)
+{
+    std::cout<<Name<<": ";
+    for (auto i = vec.begin(); i != vec.end(); ++i)
+    {
+        if (i != vec.begin()) std::cout<<", ";
+        std::cout<<*i;
+    }
+    std::cout<<"\n";
+}
+
+
+std::vector<float> generateCos(float frequency, int sample_size, float sample_rate)
+{
+    std::vector<float> cos_signal;
+    float step_size = 1/sample_rate;
+    for (float t=0; t<sample_size; t++)
+    {
+        float cos_point = cos(2 * M_PI * frequency * t * step_size);
+        cos_signal.push_back(cos_point);
+    }
+    return cos_signal;
+}
+
+
+std::vector<float> generateSin(float frequency, int sample_size, float sample_rate)
+{
+    std::vector<float> sin_signal;
+    float step_size = 1/sample_rate;
+    for (float t=0; t<sample_size; t++)
+    {
+        float sin_point = sin(2 * M_PI * frequency * t * step_size);
+        sin_signal.push_back(sin_point);
+    }
+    return sin_signal;
+}
+
+
+std::vector<float> modulateQAM(std::vector<int> &data, std::vector<float> &I_carrier, std::vector<float> &Q_carrier)
 {
     std::cout<<"Modulating Signal..."<<std::endl;
 
@@ -103,22 +103,19 @@ std::vector<float> modulateQAM(std::vector<int> &data, std::vector<float> &carri
     }
 
     // Map the 0's and 1's of the I and Q signals into +-voltages
-    float voltage = 42;
+    float voltage = 5;
     for (int j=0; j<(data.size()/2); j++)
     {
         I_signal[j] = voltage*((2*I_signal[j]) - 1);
         Q_signal[j] = voltage*((2*Q_signal[j]) - 1);
     }
 
-    std::cout<<"In-Phase Signal: ";
-    printOutVector(I_signal);
-    std::cout<<"Quadrature Signal: ";
-    printOutVector(Q_signal);
+    printOutVector(I_signal, "In-Phase Signal");
+    printOutVector(Q_signal, "Quadrature Signal");
 
-    // Mix our signals with our carrier
-    std::cout<<"Carrier Signal: ";
-    printOutVector(carrier);
-    
+    // Mix our signals with our carrier signals
+    printOutVector(I_carrier, "In-Phase Carrier");
+    printOutVector(Q_carrier, "Quadrature Carrier");
 
     
     
